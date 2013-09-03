b8f5349d30e6ee81126d15f3842d7a11e4d36ff1
Eric VanWieren <evanwieren@gmail.com>
2013-09-03 15:32:24 -0400

    Changing the structure. Going to support oauth and version 2.0 of the api.

---
+++ lib/rs_api_helper.rb
@@ -0,0 +1,12 @@
+# Starting simple this time. 
+# 
+# Will need to create a rs_api_helper
+#
+# From here you can make calls and connections.
+
+#$:.unshift File.direname(__FILE__)
+
+module RS_API_Helper
+	
+
+
---
+++ lib/rs_api_helper/connection.rb
@@ -0,0 +1,7 @@
+module RS_API_Helper
+	class API
+		def initialize(options = {})
+			# Need to add more code here
+		end
+	end
+end
